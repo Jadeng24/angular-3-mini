4bc77f182c6094f235f95b314e955a915df47b5f
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-11-02 15:04:55 -0600

    finished project

---
+++ app/cars/cars.component.js
@@ -0,0 +1,10 @@
+angular.module('fourWheels').component('cars', {
+    templateUrl: 'app/cars/cars.template.html',
+    controllerAs: 'carsCtrl',
+    controller: function (carsSrvc) {
+        this.buyCar = function (id) {
+            carsSrvc.buyCar(id);
+            this.cars = carsSrvc.cars;
+        }
+    }
+})
